5acdc69be3e1ba1e121fd04f70e6cb2e2ea265e0
Daniel Winston <disabledfishinabarrel@gmail.com>
2020-12-12 20:27:02 -0700

    Fixed Card.getRank() so that Integer.compareTo can be used. Beginning work on main game loop structure.

--- src/ThisMeansWar/Card.java
+++ src/ThisMeansWar/Card.java
@@ -10,6 +10,13 @@ public class Card
 		this.s = s;
 		this.r = r;
 	}
+	
+	public Integer getRank()
+	{
+		//enum.ordinal() returns index of suit in Suit.java, starts at 0 so I add 1
+			// Integer wrapper cast so that compareTo() functions
+		return (Integer) r.ordinal() + 1;
+	}
 
 	@Override
 	public String toString()
--- src/ThisMeansWar/Game.java
+++ src/ThisMeansWar/Game.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 
 public class Game
 {
-	//Singleton Pattern
-	private static Deck deck;
 
 	public static void main(String[] args)
 	{
@@ -13,13 +11,35 @@ public class Game
 		PlayerListFactory playerFactory = new PlayerListFactory();
 		//Create Two Players
 		ArrayList<Player> players = playerFactory.getPlayerList(2);
-		Player curr = players.get(0);
-		for (int s = 0; s < 27; s++)
-			System.out.println(curr.getCard());
-		// reveal one card from each player
+		//Player curr = players.get(0);
+		ArrayList<Card> round = new ArrayList<Card>();
 		
-		// check if one is greater than the other, winner
-			//else they are equal, draw again same conditional
+		// reveal one card from each player
+		round.add(players.get(0).getCard());
+		round.add(players.get(1).getCard());
+		System.out.println("Player 1 plays " + round.get(0) + "\nPlayer 2 plays " + round.get(1));
+		
+		int compare = (round.get(0).getRank().compareTo(round.get(1).getRank()));
+		
+		switch(compare)
+		{
+			case (0):
+				//Draw again
+				break;
+			case (1):
+				//Player 1 wins
+				System.out.println("Player 1 wins");
+				players.get(0).addCards(round);
+				round.clear();
+				break;
+			case (-1):
+				//Player 2 wins
+				System.out.println("Player 2 wins");
+				players.get(1).addCards(round);
+				round.clear();
+				break;
+		}
+		
 	}
 
 }
